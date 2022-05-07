3df6a3418d7e1cc51043e6cf2459e66f51b6123a
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-07 14:08:34 -0400

    initiated js functionality

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var container = document.querySelector(".container");
+
+container.addEventListener("click", function(event) {
+    // this will track where the click actually happens within the container
+    var element = event.target;
+
+    if (element.matches("#start-button")) {
+        // make it hidden
+
+        // make question-1 visible
+
+        // all the text in the html doc will have to be stored in data sets
+    }
+})

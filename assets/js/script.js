59b634420f607896a35c61e472dd255afafc770a
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-13 15:36:15 -0400

    added functionality of start button and countdown timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,35 @@
-// load #get-started on page load
-var getStarted = document.querySelector("#get-started")
-    //eventListener on page load ???
-    element.setAttribute("data-visibility" , "visible");
+var timerEl = document.getElementById('countdown');
+
+// function for countdown timer
+function countdown() {
+    var timeLeft = 10;
+    // create a countdown interal to performa a (function , every x miliseconds)
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+        } else if (timeLeft === 1) {
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+        } else {
+        timerEl.textContent = 'GAME OVER';
+        clearInterval(timeInterval);
+        displayMessage();
+        // ** ADD SOMETHING TO CLEAR QUESTIONS/SHOW FINAL SCORE WITH PENALTY
+          }
+        }, 1000);
+    console.log(">>> begin countdown >>>")
+}
+
+// start game on click, begin countdown
+$("#get-started").on("click" , "#start-button" , function() {
+    var startGame = $(this)
+      $("#get-started").addClass("d-none")
+      console.log(">>> start button clicked >>>")
+    // callback countdown timer
+    countdown();
+  });
+
 
 var container = document.querySelector(".container");
 
@@ -17,5 +45,4 @@ container.addEventListener("click", function(event) {
         // all the text in the html doc will have to be stored in data sets
     }
 });
-
 

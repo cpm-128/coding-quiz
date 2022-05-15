e41908f51ffb071ddc4b918814998de096176f13
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-14 22:30:04 -0400

    created questionsOnb with nested arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,36 @@
 // define variables
 var timerEl = document.getElementById('countdown');
-var timePenaltyEl = document.getElementById('time-penalty');
-var correctAnswerClick = correctAnswers [
-    
-]
+var questionsObj = [
+  {
+    question1: "Commonly used data types do NOT incldue:" ,
+      answers: ["strings" , "booleans" , "alerts" , "numbers"],
+      correct: "alerts"
+  },
+  {
+    question2: "The condition in an if/else statement is enclposed with ____________." ,
+      answers: ["quotes" , "curly brackets" , "parenthesis" , "square brackets"],
+      correct: "parenthesis"
+  },
+  {
+    question3: "Arrays in JavaScript can be used to store ____________." ,
+      answers: ["numbers and strings" , "other arrays" , "booleans" , "all of the above"],
+      correct: "all of the above"
+  },
+  {
+    question4: "String values must be enclosed within ____________ when being assigned to variables." ,
+      answers: ["commas" , "curly brackets" , "quotes" , "parenthesis"],
+      correct: "quotes"
+  },
+  {
+    question5: "A very useful tool used during development and debugging for printing content to the debugger is:" ,
+      answers: ["JavaScript" , "terminal/bash" , "for loops" , "console.log"],
+      correct: "console.log"
+  },
+];
+//correct answers: questionsObj[0].correct
+// logic to display questions, logic with event listens if/else based on correct or incorrect
+
+
 // function for countdown timer
 function countdown() {
     var timeLeft = 10;
@@ -22,23 +49,14 @@ function countdown() {
           }
         }, 1000);
     console.log(">>> begin countdown >>>")
-}
+};
 // end countdown timer
 
-// function for time penalty
-function timePenalty() {
-    var penaltyLeft = 5;
-    var penaltyInterval = setInterval(function () {
-        if (penaltyLeft > 0) {
-            timePenaltyEl.textContent = penaltyLeft;
-            penaltyLeft--;
-        } else {
-            clearInterval(penaltyInterval);
-        }
-    }, 1000);
-    // ** ADD TBD - at the end of the time penalty, should move to next question; this will probably be in a for loop? not here
+// function to loop through quiz
+function quizPlay() {
+  console.log("questionsObj[0].question1");
 };
-// end time penalty
+// end function to loop through quiz
 
 // start game on click, begin countdown, display question 1
 $("#get-started").on("click" , "#start-button" , function() {
@@ -47,36 +65,9 @@ $("#get-started").on("click" , "#start-button" , function() {
       console.log(">>> start button clicked >>>")
     // callback countdown timer
     countdown();
-    // display question 1
-    $("#question-1").removeClass("d-none");
+    // display row that contains quiz formatting
+    $("#quiz").removeClass("d-none")
+    // display questions
+    quizPlay();
   });
 // end start game button functionality
-
-// start check for correctness
-// on click within class="multiple-choice-list"
-    // if button class="correct-answer"
-        // display feedback-correct
-        // wait 0.5 second
-    // else
-        // display feedback-incorrect
-        // timepenalty(); to countodwn the 5-second penalty
-    // next question
-
-$(".multiple-choice-list").on("click" , ".button" , function() {
-        if {
-            
-        }
-      });
-//end check for correctness
-
-// loop through the quesions
-// start with question 1 - whatever
-// if button clicked, correctness();
-// loop through questions
-// end loop through the questions
-
-// score display
-// after the questions loop, display their score
-// enter name to save
-// send to local storage: name + timeLeft or score
-// end score display

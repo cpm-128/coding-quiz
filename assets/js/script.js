9aad2e259bd67b9c10dda499744fe137713b1dca
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-15 00:19:26 -0400

    updating things that are working better

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,31 @@
 // define variables
 var timerEl = document.getElementById('countdown');
+var timeLeft = 100;
+var timePenalty = 5;
+var currentQuestionIndex = 0;
 var questionsObj = [
   {
-    question1: "Commonly used data types do NOT incldue:" ,
+    question: "Commonly used data types do NOT incldue:" ,
       answers: ["strings" , "booleans" , "alerts" , "numbers"],
       correct: "alerts"
   },
   {
-    question2: "The condition in an if/else statement is enclposed with ____________." ,
+    question: "The condition in an if/else statement is enclposed with ____________." ,
       answers: ["quotes" , "curly brackets" , "parenthesis" , "square brackets"],
       correct: "parenthesis"
   },
   {
-    question3: "Arrays in JavaScript can be used to store ____________." ,
+    question: "Arrays in JavaScript can be used to store ____________." ,
       answers: ["numbers and strings" , "other arrays" , "booleans" , "all of the above"],
       correct: "all of the above"
   },
   {
-    question4: "String values must be enclosed within ____________ when being assigned to variables." ,
+    question: "String values must be enclosed within ____________ when being assigned to variables." ,
       answers: ["commas" , "curly brackets" , "quotes" , "parenthesis"],
       correct: "quotes"
   },
   {
-    question5: "A very useful tool used during development and debugging for printing content to the debugger is:" ,
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:" ,
       answers: ["JavaScript" , "terminal/bash" , "for loops" , "console.log"],
       correct: "console.log"
   },
@@ -33,7 +36,6 @@ var questionsObj = [
 
 // function for countdown timer
 function countdown() {
-    var timeLeft = 10;
     // create a countdown interal to performa a (function , every x miliseconds)
     var timeInterval = setInterval(function () {
         if (timeLeft > 1) {
@@ -54,9 +56,35 @@ function countdown() {
 
 // function to loop through quiz
 function quizPlay() {
-  console.log("questionsObj[0].question1");
-};
+  $(".question").append(questionsObj[currentQuestionIndex].question);
+    $("#button1").append(questionsObj[currentQuestionIndex].answers[0]);
+    $("#button2").append(questionsObj[currentQuestionIndex].answers[1]);
+    $("#button3").append(questionsObj[currentQuestionIndex].answers[2]);
+    $("#button4").append(questionsObj[currentQuestionIndex].answers[3]);
+  };
 // end function to loop through quiz
+
+// start questionOne
+function questionOne() {
+  console.log(currentQuestionIndex);
+  quizPlay();
+  //vwhere do they click?
+  $(".multiple-choice-list").on("click" , "#button1" , function() {
+    console.log(">>> incorrect >>>") ,
+    currentQuestionIndex++;
+    timeLeft -= timePenalty;
+  });
+  $(".multiple-choice-list").on("click" , "#button2" , function() {
+    console.log(">>> incorrect >>>")
+  })
+  $(".multiple-choice-list").on("click" , "#button3" , function() {
+    console.log(">>> correct >>>")
+  })
+  $(".multiple-choice-list").on("click" , "#button4" , function() {
+    console.log(">>> incorrect >>>")
+  })
+}
+// end questionOne
 
 // start game on click, begin countdown, display question 1
 $("#get-started").on("click" , "#start-button" , function() {
@@ -68,6 +96,6 @@ $("#get-started").on("click" , "#start-button" , function() {
     // display row that contains quiz formatting
     $("#quiz").removeClass("d-none")
     // display questions
-    quizPlay();
+    questionOne();
   });
 // end start game button functionality
